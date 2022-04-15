8790efc257b4baff4a1f301b55496a3944977f17
RogelioShelaMay <shelamayrogelio@gmail.com>
2022-04-15 10:01:20 +0700

    php song script added

---
+++ song3.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>MUSIKERO LIFESTYLE</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption">Music makes you feel free</span>
+		</div>
+	</div>
+	<div class="lyrics">
+		<?php
+				echo "<br/>";
+				echo "<br/>";
+				echo "<h2 class='text-primary'> 12:51 </h2>";
+				echo "<p class='lead'> Krissy & Ericka </p>";
+				echo "<br/>";
+
+				echo "<p> [Verse] </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> D    &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  A </p>";
+				echo "<p> Scrolling through my cellphone for the 20th time today </p>";
+				echo "<p> Em </p>";
+				echo "<p> Reading that text you sent me again </p>";
+				echo "<p> G  </p>";
+				echo "<p> Though I memorized it anyway </p>";
+				echo "<p> D </p>";
+				echo "<p> It was an afternoon in December  </p>";
+				echo "<p> A </p>";
+				echo "<p> When it reminded you of the day </p>";
+				echo "<p> Em </p>";
+				echo "<p> When we bumped into each other </p>";
+				echo "<p> G </p>";
+				echo "<p> But you didn't say hi cause I looked away </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> [Bridge]  </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D      &nbsp  &nbsp &nbsp &nbsp &nbsp    G </p>";
+				echo "<p> And maybe that was the biggest mistake of my life </p>";
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D      &nbsp  &nbsp &nbsp &nbsp &nbsp    G </p>";
+				echo "<p> And maybe I haven't moved on since that night </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p> [Chorus] </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p>            D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A </p>";
+				echo "<p> Cause it's 12:51 and I thought my feelings were gone </p>";
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &    G</p>";
+				echo "<p> But I'm lying on my bed, thinking of you again </p>";
+				echo "<p> D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp A </p>";
+				echo "<p> And the moon shines so bright, but I gotta dry these tears tonight </p>";
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &    G </p>";
+				echo "<p> Cause you're moving on and I'm not that strong to hold on any longer </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> [Interlude] </p>";
+				echo "<br/>";
+
+
+				echo "<p> D &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  A &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   Em  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G</p>";
+				echo "<p> Oh Oh Oh Oh...  </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> [Verse] </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> D  </p>";
+				echo "<p>  Then I saw you with herv </p>";
+				echo "<p> A </p>";
+				echo "<p> Didn't think you'd find another </p>";
+				echo "<p> Em  </p>";
+				echo "<p> And the world just seemed to crash </p>";
+				echo "<p> G </p>";
+				echo "<p> Shouldn't have thought this would last </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p> [Bridge]  </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D      &nbsp  &nbsp &nbsp &nbsp &nbsp    G </p>";
+				echo "<p> And maybe that was the biggest mistake of my life </p>";
+				echo "<p> Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp D      &nbsp  &nbsp &nbsp &nbsp &nbsp    G </p>";
+				echo "<p> And maybe I haven't moved on since that night </p>";
+				echo "<br/>";
+
+				echo "<p> [Verse] </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+				echo "<p> D   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   A</p>";
+				echo "<p> As the sky outside gets brighter </p>";
+				echo "<p> D              A &nbsp &nbsp Em </p>";
+				echo "<p> And my eyes begin to tire</p>";
+				echo "<p> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp G </p>";
+				echo "<p> I'm slowly drowning </p>";
+				echo "<p> In memories of him </p>";
+				echo "<p> D    &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  A </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+				echo "<p> And I know it shouldn't matter  </p>";
+				echo "<p>                            Em </p>";
+				echo "<p> As my heart begins to shatter </p>";
+				echo "<p> G </p>";
+				echo "<p> I'm left to wonder </p>";
+				echo "<p>Just how it should have been... yeah... </p>";
+				echo "<br/>";
+
+
+				echo "<p> [Outro] </p>";
+				echo "<p>D   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   A </p>";
+				echo "<p> 12:51 and I thought my feelings were gone </p>";
+				echo "<p>           Em  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp   G </p>";
+				echo "<p> But I'm lying on my bed, thinking of you again </p>";
+				echo "<p>         D        &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp      A </p>";
+				echo "<p> And the moon shines so bright, but I gotta dry these tears tonight </p>";
+				echo "<p>              Em     &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  G</p>";
+				echo "<p> Cause you're moving on and I'm not that strong to hold on </p>";
+				echo "<p>Em   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp    G</p>";
+				echo "<p> Cause I'll prove you wrong that I can move on through this song </p>";
+				echo "<p> D </p>";
+				echo "<p> So much stronger </p>";
+				echo "<br/>";
+				echo "<br/>";
+
+
+
+
+
+		?>
+		</div>		
+	</div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Shela May Rogelio
+		</div>
+	</footer>
+
+</body>
+</html>
