21efbb50bac415659ff73f60a9e662382df8c758
RogelioShelaMay <shelamayrogelio@gmail.com>
2022-04-15 11:26:00 +0700

    php song upgraded

---
+++ song4.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MELOMANIAC</title>
+</head>
+<body>
+	<nav>
+		<div class="brand_name">
+			<p>MUSIKERO LIFESTYLE</p>
+		</div>
+		<ul>
+			<li><a href="song1.php">SONG 1</a></li>
+			<li><a href="song2.php">SONG 2</a></li>
+			<li><a href="song3.php">SONG 3</a></li>
+			<li><a href="song4.php">SONG 4</a></li>
+			<li><a href="song5.php">SONG 5</a></li>
+		</ul>
+	</nav>
+	<div class="image">
+		<div class="container">
+			<h1><span>WELCOME TO MELOMANIAC</span></h1>
+			<span class="caption">Music makes you feel free</span>
+		</div>
+	</div>
+	<div class="lyrics">
+		 <?php
+
+		 		echo "<br/>";
+				echo "<br/>";
+				echo "<h2 class='text-primary'> Unwell  </h2>";
+				echo "<p class='lead'> Matchbox Twenty </p>";
+				echo "<br/>";
+
+			 	echo "<p> [Intro] </p>";
+			 	echo "<p> A D F#m E (2x) </p>";
+			 	echo "<br/>"; 
+
+
+			 	echo "<p>[Verse] </p>";
+			 	echo "<br/>";
+
+			 	echo "<p> D  &nbsp &nbsp &nbsp &nbsp E </p>";
+			 	echo "<p> All day </p>";
+			 	echo "<p> A   &nbsp &nbsp &nbsp &nbsp   F#m</p>";
+			 	echo "<p> Staring at the ceiling making </p>";
+				echo "<p> D &nbsp &nbsp &nbsp &nbsp  E   &nbsp &nbsp &nbsp &nbsp   F#m </p>";
+				echo "<p> Friends with shadows on my wall </p>";
+				 echo "<p> D &nbsp &nbsp &nbsp &nbsp E </p>";
+				 echo "<p> All night</p>";
+				 echo "<p> A   &nbsp &nbsp &nbsp &nbsp  F#m</p>";
+				 echo "<p> I'm hearing voices telling me </p>";
+				 echo "<p> D </p>";
+				 echo "<p> That I should get some sleep</p>";
+				 echo "<p>           E </p>";
+				 echo "<p> Because tomorrow might be good for something  </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+
+				 echo "<p> [Pre-Chorus] </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+
+				 echo "<p> C#m  &nbsp &nbsp &nbsp &nbsp D </p>";
+				 echo "<p> Hold on   </p>";
+				 echo "<p>       A     &nbsp &nbsp &nbsp &nbsp   E </p>";
+				 echo "<p> I'm feeling like I'm headed for a  </p>";
+				 echo "<p> C#m   &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> Breakdown </p>";
+				 echo "<p> A &nbsp &nbsp &nbsp &nbsp   E </p>";
+				 echo "<p> I don't know why </p>";
+
+
+				 echo "<p> [Chorus] </p>";
+				 echo "<br/>";
+
+
+
+				 echo "<p> A     &nbsp &nbsp &nbsp &nbsp        D </p>";
+				 echo "<p> I'm not crazy I'm just a little unwell </p>";
+				 echo "<p> I know right now you can't tell</p>";
+				 echo "<p> F#m   &nbsp &nbsp &nbsp &nbsp     E</p>";
+				 echo "<p> But stay a while and maybe then you'll see </p>";
+				 echo "<p> A diffirent side of me </p>";
+				 echo "<p> A      &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> I'm not craz im just a little impaired </p>";
+				 echo "<p> I know right you don't care </p>";
+				 echo "<p> F#m     &nbsp &nbsp &nbsp &nbsp        E </p>";
+				 echo "<p> But soon enough you gonna think of me </p>";
+				 echo "<p>       D &nbsp &nbsp &nbsp &nbsp   E </p>";
+				 echo "<p> And how i used to be me </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+
+				 echo "<p> [Verse] </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+				 echo "<p> A  &nbsp &nbsp &nbsp &nbsp   F#m </p>";
+				 echo "<p> Talking to myself in public  </p>";
+				 echo "<p> D  &nbsp &nbsp &nbsp &nbsp    E   &nbsp &nbsp &nbsp &nbsp    F#m </p>";
+				 echo "<p> And dodging glances on the train </p>";
+				 echo "<p> D  &nbsp &nbsp &nbsp &nbsp    E </p>";
+				 echo "<p> I know </p>";
+				 echo "<p> A    &nbsp &nbsp &nbsp &nbsp     F#m </p>";
+				 echo "<p> I know they've all been talking 'bout me </p>";
+				 echo "<p> D  </p>";
+				 echo "<p> I can hear them whisper</p>";
+				 echo "<p> E </p>";
+				 echo "<p> And it makes me think there must be something wrong </p>";
+				 echo "<p> C#m  &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> With me </p>";
+				 echo "<p> A  &nbsp &nbsp &nbsp &nbsp  E </p>";
+				 echo "<p> Out of all the hours thinking </p>";
+				 echo "<p> C#m &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> Somehow </p>";
+				 echo "<p> A   &nbsp &nbsp &nbsp &nbsp E </p>";
+				 echo "<p> I've lost my mind </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+				 echo "<p> [Chorus] </p>";
+				 echo "<br/>";
+
+				 echo "<p> A     &nbsp &nbsp &nbsp &nbsp        D </p>";
+				 echo "<p> I'm not crazy I'm just a little unwell </p>";
+				 echo "<p> I know right now you can't tell</p>";
+				 echo "<p> F#m   &nbsp &nbsp &nbsp &nbsp     E</p>";
+				 echo "<p> But stay a while and maybe then you'll see </p>";
+				 echo "<p> A diffirent side of me </p>";
+				 echo "<p> A      &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> I'm not craz im just a little impaired </p>";
+				 echo "<p> I know right you don't care </p>";
+				 echo "<p> F#m     &nbsp &nbsp &nbsp &nbsp        E </p>";
+				 echo "<p> But soon enough you gonna think of me </p>";
+				 echo "<p>       D &nbsp &nbsp &nbsp &nbsp   E </p>";
+				 echo "<p> And how i used to be me </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+				
+				 echo "<p> [Bridge] </p>";
+				 echo "<p> F#m   &nbsp &nbsp &nbsp &nbsp  D </p>";
+				 echo "<p> Talking in my sleepP </p>";
+				 echo "<p> F#m      &nbsp &nbsp &nbsp &nbsp        D </p>";
+				 echo "<p> Pretty soon they'll come get </p>";
+				 echo "<p> F#m     &nbsp &nbsp &nbsp &nbsp    E </p>";
+				 echo "<p> They'll taalking me away </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+				 echo "<p> [Chorus] </p>";
+				 echo "<br/>";
+
+				 echo "<p> A     &nbsp &nbsp &nbsp &nbsp        D </p>";
+				 echo "<p> I'm not crazy I'm just a little unwell </p>";
+				 echo "<p> I know right now you can't tell</p>";
+				 echo "<p> F#m   &nbsp &nbsp &nbsp &nbsp     E</p>";
+				 echo "<p> But stay a while and maybe then you'll see </p>";
+				 echo "<p> A diffirent side of me </p>";
+				 echo "<p> A      &nbsp &nbsp &nbsp &nbsp   D </p>";
+				 echo "<p> I'm not craz im just a little impaired </p>";
+				 echo "<p> I know right you don't care </p>";
+				 echo "<p> F#m     &nbsp &nbsp &nbsp &nbsp        E </p>";
+				 echo "<p> But soon enough you gonna think of me </p>";
+				 echo "<p>       D &nbsp &nbsp &nbsp &nbsp   E </p>";
+				 echo "<p> And how i used to be me </p>";
+				 echo "<br/>";
+				 echo "<br/>";
+
+
+	?>
+		</div>		
+	</div>
+	<footer>
+		<div class="footer">
+			Copyright &copy; 2022 Shela May Rogelio
+		</div>
+	</footer>
+	
+	
+
+</body>
+</html>
